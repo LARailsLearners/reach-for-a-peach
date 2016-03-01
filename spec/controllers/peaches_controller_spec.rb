339ec631dcb0ad11dc5dd1336cba549746f521af
JenDiamond <thejendiamond@gmail.com>
2016-02-29 21:07:50 -0800

    Update peaches_controller_spec.rb

--- spec/controllers/peaches_controller_spec.rb
+++ spec/controllers/peaches_controller_spec.rb
@@ -1,21 +1,5 @@
 require 'rails_helper'
 
-# RSpec.describe PeachesController, type: :controller do
-
-#   describe "GET #index" do
-#     it "returns http success" do
-#       get :index
-#       expect(response).to have_http_status(:success)
-#     end
-
-#     it "returns a peach" do
-#       get :index
-#       peach = FactoryGirl.create(:peach, name: "Find a job", deadline: 5 )
-#       expect(peach.count).to eq(1)
-#     end
-#   end
-# end
-
 RSpec.describe PeachesController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
